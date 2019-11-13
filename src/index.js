cfcc4fb70cc744885b04212d76ebc5b08aa3cfbd
Jose Luis Sandoval <jl_sandoval_salas@hotmail.com>
2019-11-13 16:24:43 -0600

    added linter and deploy with now

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Curso avanzado de Platzi</h1>, document.getElementById('app'));
+ReactDOM.render(<h1>Petgram</h1>, document.getElementById('app'))
