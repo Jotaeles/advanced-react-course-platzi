2a49b9251b2d88c863688b752036fa798abf90cb
Jose Luis Sandoval <jl_sandoval_salas@hotmail.com>
2019-11-14 11:28:25 -0600

    Added logo -> svg to component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { ListOfCategory } from './components/ListOfCategory'
 import { GlobalStyle } from './GlobalStyles'
 import { PhotoCard } from './components/PhotoCard'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
     <div>
         <GlobalStyle />
+        <Logo />
         <ListOfCategory />
         <ListOfPhotoCards />
     </div>
