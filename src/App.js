051207b864f6a1a9c5bb9adda2b7d6255ac59b48
Jose Luis Sandoval <jl_sandoval_salas@hotmail.com>
2019-11-14 10:26:51 -0600

    Added react-icon and component PhotoCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { ListOfCategory } from './components/ListOfCategory'
 import { GlobalStyle } from './GlobalStyles'
+import { PhotoCard } from './components/PhotoCard'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 export const App = () => (
     <div>
         <GlobalStyle />
         <ListOfCategory />
+        <ListOfPhotoCards />
     </div>
 )
