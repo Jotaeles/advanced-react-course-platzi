f9a035a6486024237f94bd3458f228fd55f74415
Jose Luis Sandoval <jl_sandoval_salas@hotmail.com>
2019-11-20 13:17:38 -0600

    Added first Hook

--- src/components/ListOfCategory/index.js
+++ src/components/ListOfCategory/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './style'
-import { categories } from '../../../api/db.json'
 
 export  const ListOfCategory = () => {
+    const [categories, setCategories] = useState(mockCategories)
+
+    useEffect(function () {
+        window.fetch('https://petgram-server-jl.now.sh/categories')
+            .then(res => res.json())
+            .then(response => {
+                setCategories(response)
+            })
+        }, [])
+
     return (
         <List>
             {
