adcd99d12eae3f1fffaad85e78ac7f177937ab0a
Jose Luis Sandoval <jl_sandoval_salas@hotmail.com>
2019-11-21 17:18:21 -0600

    Added useEffect clean events

--- src/components/ListOfCategory/index.js
+++ src/components/ListOfCategory/index.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './style'
 
 export  const ListOfCategory = () => {
-    const [categories, setCategories] = useState(mockCategories)
+    const [categories, setCategories] = useState([])
+    const [showFixed, setShowFixed] = useState(false)
 
     useEffect(function () {
         window.fetch('https://petgram-server-jl.now.sh/categories')
@@ -13,11 +14,30 @@ export  const ListOfCategory = () => {
             })
         }, [])
 
-    return (
-        <List>
+    useEffect(function () {
+        const onScroll = e => {
+            const newShowFixed = window.scrollY > 200
+            showFixed !== newShowFixed && setShowFixed(newShowFixed)
+        }
+
+        document.addEventListener('scroll', onScroll)
+
+        return () => document.removeEventListener('scroll', onScroll)
+
+    }, [showFixed])
+
+    const renderList = (fixed) => (
+        <List className={fixed ? 'fixed' : ''}>
             {
                 categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
             }
         </List>
+    )
+
+    return (
+        <Fragment>
+            {renderList()}
+            {showFixed && renderList(true)}
+        </Fragment>
     )
 }
